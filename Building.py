14a4fed256dd074c64d888e1503f47860ee0a7f1
Roey Feingold <roey.feingold@gmail.com>
2021-11-11 16:39:42 +0200

    Update files to git

--- Building.py
+++ Building.py
@@ -1,5 +1,7 @@
 from Elevator import *
 import json
+
+
 class Building:
     def __init__(self, jFile):
         self.myElevators = []
@@ -10,7 +12,8 @@ class Building:
             self.maxFloor = data['_maxFloor']
             elev = data["_elevators"]
             for elev in data['_elevators']:
-                tempElev = Elevator(elev['_id'],elev['_speed'],elev['_minFloor'],elev['_maxFloor'],elev['_closeTime'],elev['_openTime'],elev['_startTime'],elev['_stopTime'])
+                tempElev = Elevator(elev['_id'], elev['_speed'], elev['_minFloor'], elev['_maxFloor'],
+                                    elev['_closeTime'], elev['_openTime'], elev['_startTime'], elev['_stopTime'])
                 self.myElevators.append(tempElev)
             f.close()
         except FileNotFoundError:
@@ -24,10 +27,11 @@ class Building:
 
     def __str__(self):
         try:
-            myString = 'min floor is: ' + str(self.minFloor) + '\nmax floor is :' + str(self.maxFloor) + '\n------------------------------------------\n'
+            myString = 'min floor is: ' + str(self.minFloor) + '\nmax floor is :' + str(
+                self.maxFloor) + '\n------------------------------------------\n'
             for elev in self.myElevators:
                 tempString = elev.__str__() + '\n'
-                myString = myString + tempString +'------------------------------------------\n'
+                myString = myString + tempString + '------------------------------------------\n'
             return myString
         except AttributeError:
             return 'Nothing to print'
--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import json
 from Elevator import *
 from Building import *
+from data.Ex1_input.Ex1_Buildings import *
+
+
 # This is a sample Python script.
 
 # Press Shift+F10 to execute it or replace it with your code.
@@ -14,20 +17,22 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    building1 = Building('C:\\Users\\Matanel\\PycharmProjects\\OOP_2021-main\\OOP_2021-main\\Assignments\\Ex1-Git\\data\\Ex1_input\\Ex1_Buildings\\B5.json')
-    print(building1.getminfloor())
+    # building1 = Building('C:\\Users\\Matanel\\PycharmProjects\\OOP_2021-main\\OOP_2021-main\\Assignments\\Ex1-Git\\data\\Ex1_input\\Ex1_Buildings\\B5.json')
+    building1 = Building('C:\\Users\\roeyf\\Documents\\GitHub\\OOP_Ex1\\data\\Ex1_input\\Ex1_Buildings\\B1.json')
+    # building1 = Building('B1.json')
+    print('original building1.getminfloor()=', building1.getminfloor())
     building1.minFloor = -65132468
-    print(building1.getminfloor())
-    print(building1.getmaxfloor())
-    #print(building1.)
-    #print(building1)
-    #f = open('B2.json')
-    #data = json.load(f)
-    #elev = data["_elevators"]
-    #print(elev[0]["_speed"])
-    #print(elev[1])
-    #elev1 = Elevator(elev[0]["_id"],elev[0]["_speed"],elev[0]["_minFloor"],elev[0]["_maxFloor"],elev[0]["_closeTime"],elev[0]["_openTime"],elev[0]["_startTime"],elev[0]["_stopTime"])
+    print('new value building1.getminfloor()=', building1.getminfloor())
+    print('original building1.getmaxfloor()=', building1.getmaxfloor())
+    # print(building1.)
+    # print(building1)
+    # f = open('B2.json')
+    # data = json.load(f)
+    # elev = data["_elevators"]
+    # print(elev[0]["_speed"])
+    # print(elev[1])
+    # elev1 = Elevator(elev[0]["_id"],elev[0]["_speed"],elev[0]["_minFloor"],elev[0]["_maxFloor"],elev[0]["_closeTime"],elev[0]["_openTime"],elev[0]["_startTime"],elev[0]["_stopTime"])
     # print(elev1)
-    #elev1= Elevator(1,10,-10,100,1,1,1,1)
-    #print(elev1)
+    # elev1= Elevator(1,10,-10,100,1,1,1,1)
+    # print(elev1)
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
