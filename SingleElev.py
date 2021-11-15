7abe82212989620e8b961093e2476815b8862046
Roey Feingold <roey.feingold@gmail.com>
2021-11-15 17:28:21 +0200

    Update SingleElev

--- SingleElev.py
+++ SingleElev.py
@@ -1,7 +1,7 @@
 from Building import *
 
 
-class SinglgeElev:
+class SingleElev:
     # def __init__(self, UpCalls, DownCalls, elevid, speed, minfloor, maxfloor, closetime, opentime, starttime, stoptime):
     #     self.elevid = elevid
     #     self.speed = speed
@@ -13,9 +13,28 @@ class SinglgeElev:
     #     self.stopTime = stoptime
     #     self.UpCalls = UpCalls
     #     self.DownCalls = DownCalls
-    myUpCalls = []
-    myDownCalls = []
+
     jsonFile = 'C:\\Users\\roeyf\\Documents\\GitHub\\OOP_Ex1\\data\\Ex1_input\\Ex1_Buildings\\B1.json'
     csvFile = 'C:\\Users\\roeyf\\Documents\\GitHub\\OOP_Ex1\\data\\Ex1_input\\Ex1_Calls\\Calls_a.csv'
     myBuilding = Building(jsonFile, csvFile)
-    if myBuilding.myElevators.__sizeof__() == 1:
+    allCalls = myBuilding.myCalls
+    UpCalls = myBuilding.up_calls
+    DownCalls = myBuilding.down_calls
+    myDirCalls = []
+    if allCalls[0].call_dir == True:
+        myDirCalls = UpCalls
+    else:
+        myDirCalls = DownCalls
+    myCalls = []
+    up = allCalls[0].call_dir
+    tempC = None
+    for i in myDirCalls.__sizeof__():
+        if i == myDirCalls[0]:
+            myCalls.append(myDirCalls[0])
+            tempC = myDirCalls[0]
+        elif up & tempC.srcFloor < myDirCalls[i + 1].srcFloor:
+            myCalls.append(myDirCalls[i])
+            tempC = myDirCalls[i]
+        elif (not up) & tempC.srcFloor > myDirCalls[i + 1].srcFloor:
+            myCalls.append(myDirCalls[i])
+            tempC = myDirCalls[i]
