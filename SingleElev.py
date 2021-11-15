9c1dbb4c59e000812c7755969501c6eeabc2cdcb
Roey Feingold <roey.feingold@gmail.com>
2021-11-15 16:48:26 +0200

    SingleElev + Up&Down Calls list 1

--- SingleElev.py
+++ SingleElev.py
@@ -1,15 +1,20 @@
+from Building import  *
 class SinglgeElev:
 
-    def __init__(self, UpCalls, DownCalls, elevid, speed, minfloor, maxfloor, closetime, opentime, starttime, stoptime):
-        self.elevid = elevid
-        self.speed = speed
-        self.minFloor = minfloor
-        self.maxFloor = maxfloor
-        self.closeTime = closetime
-        self.openTime = opentime
-        self.startTime = starttime
-        self.stopTime = stoptime
-        self.UpCalls = UpCalls
-        self.DownCalls = DownCalls
+    # def __init__(self, UpCalls, DownCalls, elevid, speed, minfloor, maxfloor, closetime, opentime, starttime, stoptime):
+    #     self.elevid = elevid
+    #     self.speed = speed
+    #     self.minFloor = minfloor
+    #     self.maxFloor = maxfloor
+    #     self.closeTime = closetime
+    #     self.openTime = opentime
+    #     self.startTime = starttime
+    #     self.stopTime = stoptime
+    #     self.UpCalls = UpCalls
+    #     self.DownCalls = DownCalls
+    myUpCalls= []
+    myDownCalls = []
+    jsonFile = 'C:\\Users\\roeyf\\Documents\\GitHub\\OOP_Ex1\\data\\Ex1_input\\Ex1_Buildings\\B1.json'
+    csvFile = 'C:\\Users\\roeyf\\Documents\\GitHub\\OOP_Ex1\\data\\Ex1_input\\Ex1_Calls\\Calls_a.csv'
+    myBuiding = Building(jsonFile, csvFile)
 
-    def my_calls(self):
