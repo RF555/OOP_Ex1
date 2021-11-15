bd06e25450c5d6be6f31b7d402a97b1abaeb4f5b
Roey Feingold <roey.feingold@gmail.com>
2021-11-15 14:09:32 +0200

    Update SCall & Call

--- Call.py
+++ Call.py
@@ -1,21 +1,21 @@
 class Call:
     elevIndex: int = None
     elevStatus: int = None
-    req_time = None
-    index: int = None
+    request_time = None
+    allocatedElev: int = None
     srcFloor: int = None
     destFloor: int = None
     CallStatus = 'Waiting'
     call_dir: bool  # if call is UP- TRUE, if call is DOWN- FALSE
 
-    def __init__(self, time, srcFloor, destFloor, elevStatus, elevIndex, index):
+    def __init__(self, request_time, srcFloor, destFloor, elevStatus, elevIndex, allocatedElev):
         self.elevIndex = elevIndex
         self.elevStatus = elevStatus
-        self.req_time = time
-        self.index = index
+        self.request_time = request_time
+        self.allocatedElev = allocatedElev
         self.srcFloor = srcFloor
         self.destFloor = destFloor
         self.call_dir = (self.srcFloor < self.destFloor)
 
     def __str__(self):
-        return "Elevator call," + self.req_time.__str__() + "," + self.srcFloor.__str__() + "," + self.destFloor.__str__() + ',' + self.elevStatus.__str__() + ',' + self.elevIndex.__str__()
+        return "Elevator call," + self.request_time.__str__() + "," + self.srcFloor.__str__() + "," + self.destFloor.__str__() + ',' + self.elevStatus.__str__() + ',' + self.elevIndex.__str__()
--- SCall.py
+++ SCall.py
@@ -3,13 +3,13 @@ import sys
 
 class SCall(object):
     stop_id = None
-    time = None
+    call_time = None
     floor = None  # floor (src/dest)
     type = None  # 0 if src, 1 if dest
 
     def __init__(self, Call):
         self.stop_id = (-1) * sys.maxsize
-        self.time = Call.req_time
+        self.call_time = Call.request_time
 
 
 class SrcStop(SCall):
