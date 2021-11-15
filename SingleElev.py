f036a94f231c5fd8cbb147fe03bc84af2ea24378
Roey Feingold <roey.feingold@gmail.com>
2021-11-15 16:55:41 +0200

    if matanel

--- SingleElev.py
+++ SingleElev.py
@@ -1,6 +1,7 @@
-from Building import  *
-class SinglgeElev:
+from Building import *
 
+
+class SinglgeElev:
     # def __init__(self, UpCalls, DownCalls, elevid, speed, minfloor, maxfloor, closetime, opentime, starttime, stoptime):
     #     self.elevid = elevid
     #     self.speed = speed
@@ -12,9 +13,9 @@ class SinglgeElev:
     #     self.stopTime = stoptime
     #     self.UpCalls = UpCalls
     #     self.DownCalls = DownCalls
-    myUpCalls= []
+    myUpCalls = []
     myDownCalls = []
     jsonFile = 'C:\\Users\\roeyf\\Documents\\GitHub\\OOP_Ex1\\data\\Ex1_input\\Ex1_Buildings\\B1.json'
     csvFile = 'C:\\Users\\roeyf\\Documents\\GitHub\\OOP_Ex1\\data\\Ex1_input\\Ex1_Calls\\Calls_a.csv'
     myBuiding = Building(jsonFile, csvFile)
-
+    if myBuiding.myElevators.__sizeof__() == 1:
