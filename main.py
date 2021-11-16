741b965ce6d8bcb6139829bf344f956299e8c774
Roey Feingold <roey.feingold@gmail.com>
2021-11-16 18:09:27 +0200

    Update

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import json
 
-#import SingleElev
+# import SingleElev
 from Elevator import *
 from Building import *
 from myFunction import *
+
+
 # This is a sample Python script.
 
 # Press Shift+F10 to execute it or replace it with your code.
@@ -20,8 +22,10 @@ if __name__ == '__main__':
     # building1 = Building('C:\\Users\\Matanel\\PycharmProjects\\OOP_2021-main\\OOP_2021-main\\Assignments\\Ex1-Git\\data\\Ex1_input\\Ex1_Buildings\\B5.json')
     # jsonFile = 'C:\\Users\\Matanel\\Desktop\\Ex1\\Ex1-Git\\data\\Ex1_input\\Ex1_Buildings\\B1.json'
     # csvFile = 'C:\\Users\\Matanel\\Desktop\\Ex1\\Ex1-Git\\data\\Ex1_input\\Ex1_Calls\\Calls_a.csv'
-    jsonFile='C:\\Users\\Roey\\Documents\\GitHub\\OOP_Ex1\\data\\Ex1_input\\Ex1_Buildings\\B1.json'
-    csvFile='C:\\Users\\Roey\\Documents\\GitHub\\OOP_Ex1\\data\\Ex1_input\\Ex1_Calls\\Calls_a.csv'
+    # jsonFile = 'C:\\Users\\Roey\\Documents\\GitHub\\OOP_Ex1\\data\\Ex1_input\\Ex1_Buildings\\B1.json'
+    # csvFile = 'C:\\Users\\Roey\\Documents\\GitHub\\OOP_Ex1\\data\\Ex1_input\\Ex1_Calls\\Calls_a.csv'
+    jsonFile = 'C:\\Users\\roeyf\\Documents\\GitHub\\OOP_Ex1\\data\\Ex1_input\\Ex1_Buildings\\B1.json'
+    csvFile = 'C:\\Users\\roeyf\\Documents\\GitHub\\OOP_Ex1\\data\\Ex1_input\\Ex1_Calls\\Calls_a.csv'
     building1 = Building(jsonFile, csvFile)
     temp = building1.myCalls[0]
     temp1 = building1.myCalls[1]
@@ -30,7 +34,7 @@ if __name__ == '__main__':
     myFunction.allocateElev(building1.myCalls, building1.myElevators[0], temp1)
     print(building1.myCalls[1])
     print(building1.myCalls[1].allocatedElev)
-    #Here are the test for my functions
+    # Here are the test for my functions
     ####################################################################################
     # print(temp)
     # print(temp1)
