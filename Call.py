1e6c3004e43f633ea3c1310de0224dc1e0f904dd
[Matanel1995] <[Matanel5555@gmail.com]>
2021-11-18 12:57:33 +0300

    update

--- Call.py
+++ Call.py
@@ -9,17 +9,14 @@ class Call:
     # change call_dir to int: 1 is UP, -1 is DOWN, 0 is LEVEL
     call_dir: bool  # if call is UP- TRUE, if call is DOWN- FALSE
 
-    # def __init__(self, request_time, srcFloor, destFloor, elevStatus, elevIndex, allocatedElev):
-    def __init__(self, request_time: float, srcFloor: int, destFloor: int, elevStatus, elevIndex: int,
-                 allocatedElev: int):
+    def __init__(self, request_time, srcFloor, destFloor, elevStatus, elevIndex, allocatedElev):
         self.elevIndex = elevIndex
         self.elevStatus = elevStatus
         self.request_time = request_time
         self.allocatedElev = allocatedElev
         self.srcFloor = srcFloor
         self.destFloor = destFloor
-        # self.call_dir = (int(self.srcFloor) < int(self.destFloor))
-        self.call_dir = (self.srcFloor < self.destFloor)
+        self.call_dir = (int(self.srcFloor) < int(self.destFloor))
 
     def __str__(self):
         return "Elevator call," + self.request_time.__str__() + "," + self.srcFloor.__str__() + "," + self.destFloor.__str__() + ',' + self.elevStatus.__str__() + ',' + self.allocatedElev.__str__()
